183778f479f10b47a792266995db9683b5f0741d
HelplessSoldier <patrick1234143211@gmail.com>
2023-06-14 13:26:00 -0700

    added basic enemy movement and attacks

--- bulletHell.py
+++ bulletHell.py
@@ -9,11 +9,13 @@ SCREENWIDTH = 700
 SCREENHEIGHT = 900
 MOVEMENTSPEED = 800
 SPEEDMULTIPLIER = 0.6
+ENEMYUPDATEDELAY = 5000
 PROJECTILERADIUS = 5
-PROJECTILESPEED = 5
+PROJECTILESPEED = 4
 GRAVITY = .2
 RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
+PURPLE = (255, 0, 255)
 
 pygame.init()
 pygame.mixer.init()
@@ -49,18 +51,44 @@ class Projectile:
         pygame.draw.circle(screen, YELLOW, (self.x, self.y), radius)
 
 class Enemy(pygame.sprite.Sprite):
-    def __init__(self, screen, enemyRadius, enemyColor, spawnLocX, spawnLocY):
+    def __init__(self, screen, enemyRadius, enemyColor):
         super().__init__()
-        self.screen = screen
         self.radius = enemyRadius
         self.color = enemyColor
-        self.locX = spawnLocX
-        self.locY = spawnLocY
-    
-    def draw(self):
-        pygame.draw.circle(surface=self.screen, color=self.color, center=(self.locX, self.locY), radius=self.radius)
+        self.screen = screen
+        self.image = pygame.Surface((50, 50))
+        self.image.fill((255, 0, 0))
+        self.rect = self.image.get_rect()
+        self.position = pygame.Vector2(random.randint(100, SCREENWIDTH - 100), random.randint(100, SCREENHEIGHT * 0.35))
+        self.target = self.position
+        self.speed = 4
+        self.attack_delay = 500  # Delay between attacks in milliseconds
+        self.last_attack_time = pygame.time.get_ticks()  # Initialize last attack time
 
-def playerMovement(speed):
+    def update(self):
+        # Interpolate towards the target position
+        direction = self.target - self.position
+        distance = direction.length()
+        if distance > 0:
+            direction.normalize_ip()
+            movement = direction * min(distance, self.speed)
+            self.position += movement
+            self.rect.center = self.position
+
+        # Check if target reached, then set a new target
+        if distance <= self.speed:
+            self.target = pygame.Vector2(random.randint(100, SCREENWIDTH - 100), random.randint(100, SCREENHEIGHT * 0.35))
+
+    def attackBasic(self):
+        current_time = pygame.time.get_ticks()
+        if current_time - self.last_attack_time >= self.attack_delay:
+            self.last_attack_time = current_time
+            generateCircularProjectiles(projectiles, projectile_count=8, projectile_radius=5, sourceX=self.position[0], sourceY=self.position[1], angle=0)
+            
+    def draw(self):
+        pygame.draw.circle(surface=self.screen, color=self.color, center=(self.position[0], self.position[1]), radius=self.radius)
+
+def playerInput(speed):
     MOVEMENTSPEED = speed
     keys = pygame.key.get_pressed()
     if keys[pygame.K_z]:
@@ -97,11 +125,11 @@ def generateCircularProjectiles(projectiles, projectile_count, projectile_radius
 
         current_angle += angle_increment
         
-    for projectile in projectiles:
-        projectile.update()
-        projectile.draw(screen, projectile_radius)
-        if projectile.y <= 0:
-            projectiles.remove(projectile)
+    # for projectile in projectiles:
+    #     projectile.update()
+    #     projectile.draw(screen, projectile_radius)
+    #     if projectile.y <= 0:
+    #         projectiles.remove(projectile)
 
 def hitDetected(playerX, playerY, playerRadius, projectileX, projectileY, projectileRadius):
     distance = math.sqrt((playerX - projectileX) ** 2 + (playerY - projectileY) ** 2)
@@ -121,17 +149,22 @@ def drawLives(lives):
         y = 0  
         screen.blit(heart_image, (x, y)) 
 
+def drawProjectiles():
+    for projectile in projectiles:
+        projectile.draw(screen, PROJECTILERADIUS)
+        projectile.update()
+        
 projectiles = []
 player1 = Player(screen, 10, RED, (SCREENWIDTH / 2), (SCREENHEIGHT * 0.9))
-
 game_over = False
 game_over_timer = 0
 game_over_duration = 5000
 default_font = pygame.font.Font(pygame.font.get_default_font(), 75)
 game_over_text = default_font.render('GAME OVER', True, (255, 255, 255))
 start_time = pygame.time.get_ticks()
-# hacky shit incoming (holds endgame screen for game_over_duration)
-gameTimeThing = True
+attack_start_time = pygame.time.get_ticks()
+gameTimeThing = True # goofy hack
+enemy = Enemy(screen=screen, enemyRadius=20, enemyColor=PURPLE)
 
 # main loop
 while running:
@@ -139,14 +172,11 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     
-    playerMovement(MOVEMENTSPEED)
+    playerInput(MOVEMENTSPEED)
     playerScreenLock(player1)
-        
     screen.fill((0, 0, 0))  
-    
-    generateCircularProjectiles(projectiles, 8, PROJECTILERADIUS, random.randint(100, 600), 100, random.randint(0,20))
-    
     drawLives(LIVES)    
+    
 # hit detection
     for projectile in projectiles:
         if hitDetected(player1.playerLocX, player1.playerLocY, player1.playerRadius, projectile.x, projectile.y, PROJECTILERADIUS):
@@ -155,8 +185,7 @@ while running:
             pygame.mixer.music.load('hitsound.mp3')
             pygame.mixer.music.play()
   
- # game over condition  
- #TODO fix time/score 
+# game over condition  
     if LIVES <= 0:
         current_time = pygame.time.get_ticks()
         screen.blit(game_over_text, (SCREENWIDTH // 2 - game_over_text.get_width() // 2, SCREENHEIGHT // 2 - game_over_text.get_height() // 2))
@@ -169,6 +198,25 @@ while running:
             
         if current_time - start_time >= game_over_duration:
             running = False 
+
+# enemy movement
+    enemy_current_time = pygame.time.get_ticks()
+    if enemy_current_time - start_time >= ENEMYUPDATEDELAY:
+        enemy.target = pygame.Vector2(random.randint(50, SCREENWIDTH - 50), random.randint(100, SCREENHEIGHT * 0.3))
+        start_time = enemy_current_time
+    
+# ememy attacks
+    attack_current_time = pygame.time.get_ticks()
+    if attack_current_time - attack_start_time >= 1:
+        enemy.attackBasic()
+        attack_start_time = attack_current_time
+    
+    
+#---------------------------------------------------------------------------------------------------------
+    enemy.update()
+    enemy.draw()
+
+    drawProjectiles()
 
     player1.draw() 
     pygame.display.flip()
